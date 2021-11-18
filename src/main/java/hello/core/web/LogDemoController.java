684503cc0137542850999702cc49287204ed55e7
KDB5844 <sbjo7713@gmail.com>
2021-11-18 21:15:34 +0900

    study spring-core finish

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,13 +14,15 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURI = request.getRequestURI().toString();
+
+        System.out.println(myLogger.getClass());
+
         myLogger.setRequestURL(requestURI);
 
         myLogger.log("controller test");
